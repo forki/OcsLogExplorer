4b147d94bce382ebb4b18bf6a0cce0e431f6f0a9
Marcin Juraszek <mail@marcinjuraszek.com>
2016-06-26 11:46:11 -0700

    Configure static content for suave

---
+++ src/OcsLogExplorer.Server/WebService.fs
@@ -0,0 +1,18 @@
+﻿namespace OcsLogExplorer.Server
+
+module WebService =
+
+    open Suave
+    open Suave.Operators
+
+    let app : WebPart =
+      choose [
+        Filters.GET >=> choose [ Filters.path "/content" >=> Files.file "index.html"; Files.browseHome ]
+        RequestErrors.NOT_FOUND "Page not found." 
+        ]
+
+
+    [<EntryPoint>]
+    let Main args =
+        startWebServer defaultConfig app
+        1
